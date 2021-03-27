42bc048b23c64c53e3e1f31a537b146f0306a9aa
xomcx <omarmoyorido@gmail.com>
2021-03-26 22:59:06 -0600

    archivo prod

---
+++ empleado/settings/prod.py
@@ -0,0 +1,26 @@
+from .base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbempleado',
+        'USER': 'postgres',
+        'PASSWORD':'1e50e436b79',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
